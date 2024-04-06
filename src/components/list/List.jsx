3edc193db521b629835a6fcecd758d1834708c1b
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-07 02:47:43 +0300

    Adds docker file and implements slider new file:   .dockerignore new file:   .vscode/launch.json new file:   .vscode/tasks.json new file:   Dockerfile

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,23 +1,33 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
 import './list.scss'
 import ListItem from '../listItem/ListItem'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const List = () => {
+    const [slideNumber, setSlideNumber] = useState(0)
 
-  const listRef = useRef()
+    const listRef = useRef();
 
-  const handleClick = (direction) => {
-    if (direction === "left") {
-      console.log("test")
+    const handleClick = (direction) => {
+      let distance = listRef.current.getBoundingClientRect().x - 50
+      
+      if (direction === "left" && slideNumber > 0) {
+        setSlideNumber(slideNumber - 1)
+        listRef.current.style.transform = `translate(${230 + distance}px)`
+      }
+      if (direction === "right" && slideNumber < 5) {
+        setSlideNumber(slideNumber + 1)
+        listRef.current.style.transform = `translate(${-230 + distance}px)`
+      }
+      console.log(distance)
     }
-  }
   return (
     <div className='list'>
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
         <ArrowBackIosOutlined className='sliderArrow left' onClick={()=>handleClick("left")}/>
         <div className="container"  ref={listRef}>
+          <ListItem/>
           <ListItem/>
           <ListItem/>
           <ListItem/>

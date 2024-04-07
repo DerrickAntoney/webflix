d73402672fbb8f4d4e5cbbf5a08778eaddd3da09
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-07 05:06:39 +0300

    Adds and styles trailer video on hover

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,39 @@
+import { Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined } from '@mui/icons-material'
 import './listItem.scss'
+import { useState } from 'react'
 
-const ListItem = () => {
+const ListItem = ({index}) => {
+  const [isHovered, setIsHovered] = useState(false)
   return (
-    <div className='listItem'>
-      Item
+    <div className='listItem'
+     style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+     onMouseEnter={() => setIsHovered(true)}
+     onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src="../../../assets/cinema.jpg" alt="" />
+      {isHovered && (
+        <>
+          <video src="../../assets/videos/trailer.mp4" autoPlay={true} loop/>
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className='icon'/>
+              <Add className='icon'/>
+              <ThumbUpAltOutlined className='icon'/>
+              <ThumbDownOutlined className='icon'/>
+            </div>
+            <div className="itemInfoItem">
+              <span className="title">1hour 14mins</span>
+              <span className="limit">2021</span>
+              <span className="year">1999</span>
+            </div>
+            <div className="desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+      
     </div>
   )
 }

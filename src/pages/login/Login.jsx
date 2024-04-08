ba161eb136c51704f55858decc0f6d15d0ceefff
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-08 04:01:40 +0300

    Styles log in page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,8 +7,18 @@ const Login = () => {
         <div className="top">
             <div className="wrapper">
                 <img className='logo' src="../../../assets/netflix-3.svg" alt="page logo" />
-                <button className="signUpButton">Sign Up</button>
             </div>
+        </div>
+        <div className="container">
+            <form>
+                <h2>Sign In</h2>
+                <input type="email" placeholder='Email or phone number'/>
+                <input type="password" placeholder='Password'/>
+                <button className='loginButton'>Sign In</button>
+                <span>New to Netflix? <b>Sign up now.</b></span>
+                <small>This page is protected by Google reCAPTCHA to ensure you're not a bot. <b>Learn more</b></small>
+            </form>
+            
         </div>
         
     </div>
